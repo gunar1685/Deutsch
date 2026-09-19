e9d0ba7b858f6327c669653683ecf43aa908b500
gunar1685 <gunar.info@gmail.com>
2026-09-19 21:01:35 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Bei jeder inhaltlichen Änderung der App diese Versionsnummer erhöhen (v2, v3, ...).
 // Das zwingt Browser dazu, den Service Worker neu zu installieren statt die alte
 // Version aus dem Cache weiterzuverwenden.
-const CACHE_NAME = "deutsch-detektiv-cache-v4";
+const CACHE_NAME = "deutsch-detektiv-cache-v5";
 const ASSETS = [
   "./",
   "./index.html",
